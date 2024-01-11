8f926144777e4da7949b30cb569249e80909a569
Sérgio Rios Ribeiro <sergioriosribeiro@outlook.com>
2024-01-11 14:25:29 -0300

    Regra limite de parcelas e de data de primeira parcela e Readme

--- credit-application-system/src/main/kotlin/me/dio/creditapplicationsystem/dto/CreditDto.kt
+++ credit-application-system/src/main/kotlin/me/dio/creditapplicationsystem/dto/CreditDto.kt
@@ -1,6 +1,8 @@
 package me.dio.creditapplicationsystem.dto
 
 import jakarta.validation.constraints.Future
+import jakarta.validation.constraints.Max
+import jakarta.validation.constraints.Min
 import jakarta.validation.constraints.NotNull
 import me.dio.creditapplicationsystem.entity.Credit
 import me.dio.creditapplicationsystem.entity.Customer
@@ -10,7 +12,8 @@ import java.time.LocalDate
 data class CreditDto(
     @field:NotNull(message = "Insert a valid credit value") val creditValue: BigDecimal,
     @field:Future val dayFirstOfInstallment: LocalDate,
-    val numberOfInstallments: Int,
+    @field:Min(value = 1, message = "Invalid value. Insert a value bigger than 0 ")
+    @field:Max(value = 48, message = "Invalid value. Insert a value smaller than 49 ") val numberOfInstallments: Int,
     @field:NotNull(message = "Invalid Id. Insert a valid customer Id ") val customerId: Long
 ) {
 
