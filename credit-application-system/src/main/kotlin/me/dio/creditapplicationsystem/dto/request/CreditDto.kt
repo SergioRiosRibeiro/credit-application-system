97897aef1a385e44bbaaf1a87265c86e0ed192aa
Sérgio Rios Ribeiro <sergioriosribeiro@outlook.com>
2024-01-16 09:16:59 -0300

    Correção em códigos de testes e mudança em data de 1ª parcela

--- credit-application-system/src/main/kotlin/me/dio/creditapplicationsystem/dto/request/CreditDto.kt
+++ credit-application-system/src/main/kotlin/me/dio/creditapplicationsystem/dto/request/CreditDto.kt
@@ -1,6 +1,7 @@
 package me.dio.creditapplicationsystem.dto.request
 
 import jakarta.validation.constraints.Future
+import jakarta.validation.constraints.FutureOrPresent
 import jakarta.validation.constraints.Max
 import jakarta.validation.constraints.Min
 import jakarta.validation.constraints.NotNull
@@ -11,11 +12,24 @@ import java.time.LocalDate
 
 data class CreditDto(
     @field:NotNull(message = "Insert a valid credit value") val creditValue: BigDecimal,
-    @field:Future val dayFirstOfInstallment: LocalDate,
-    @field:Min(value = 1, message = "Invalid value. Insert a value smaller than 0")
-    @field:Max(value = 48, message = "Invalid value. Insert a value smaller than 49") val numberOfInstallments: Int,
+    @field:FutureOrPresent(message ="Insert a valid date") val dayFirstOfInstallment: LocalDate,
+    @field:Min(value = 1, message = "Invalid number of installments. Insert a value smaller than 0")
+    @field:Max(value = 48, message = "Invalid number of installments. Insert a value smaller than 49") val numberOfInstallments: Int,
     @field:NotNull(message = "Invalid Id. Insert a valid customer Id ") val customerId: Long
 ) {
+
+    init {
+        validateDates()
+    }
+
+    private fun validateDates() {
+        val currentDate = LocalDate.now()
+        val maxDate = currentDate.plusMonths(3)
+
+        if (dayFirstOfInstallment.isBefore(currentDate) || dayFirstOfInstallment.isAfter(maxDate)) {
+            throw IllegalArgumentException("The date of the first installment must be no later than 3 months after the current day")
+        }
+    }
 
     fun toEntity(): Credit = Credit(
         creditValue = this.creditValue,
